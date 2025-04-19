a108bdd6599e3d21abb152926c648f9f776d3a89
Junjie Chen <chen2468830035@gmail.com>
2025-04-20 04:28:27 +0800

    Initial commit

---
+++ src/main/java/com/example/_vs2Server/Application.java
@@ -0,0 +1,13 @@
+package com.example._vs2Server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
